8c79cea3ef5fbaa60acdd44658c99a900ae4b5d9
Ahmed Mostafa <77973041+Ambmo@users.noreply.github.com>
2022-12-30 00:30:16 +0200

    Add project files.

---
+++ App.DAL/DataContext/VotingCloudDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.DAL.DataContext;
+
+public partial class VotingCloudDbContext : DbContext
+{
+    public VotingCloudDbContext()
+    {
+    }
+
+    public VotingCloudDbContext(DbContextOptions<VotingCloudDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    { 
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
